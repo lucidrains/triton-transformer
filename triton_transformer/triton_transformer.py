4251b68569f927c7b1f9429cc5ba656cbf7b57a4
Phil Wang <lucidrains@gmail.com>
2021-09-15 14:40:39 -0700

    some more scaffold

--- triton_transformer/triton_transformer.py
+++ triton_transformer/triton_transformer.py
@@ -6,9 +6,85 @@ from einops import rearrange
 import triton
 import triton.language as tl
 
-class Transformer(nn.Module):
-    def __init__(self):
+# helpers classes
+
+class Attention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        heads = 8
+    ):
         super().__init__()
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        inner_dim = dim_head * heads
+
+        self.norm = nn.LayerNorm(dim)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        self.to_out = nn.Linear(inner_dim, dim)
 
     def forward(self, x):
-        return x
+        h = self.heads
+        x = self.norm(x)
+
+        q, k, v = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> (b h) n d', h = h), (q, k, v))
+
+        q = q * self.scale
+        sim = einsum('b i d, b j d -> b i j', q, k)
+        attn = sim.softmax(dim = -1)
+        out = einsum('b i j, b j d -> b i d', attn, v)
+        out = rearrange(out, '(b h) n d -> b n (h d)', h = h)
+        return self.to_out(out)
+
+def FeedForward(dim, mult = 4):
+    return nn.Sequential(
+        nn.LayerNorm(dim),
+        nn.Linear(dim, dim * mult),
+        nn.GELU(),
+        nn.Linear(dim * mult, dim)
+    )
+
+# main class
+
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        num_tokens,
+        max_seq_len,
+        depth,
+        heads = 8,
+        dim_head = 64
+    ):
+        super().__init__()
+        self.token_emb = nn.Embedding(num_tokens, dim)
+        self.pos_emb = nn.Embedding(max_seq_len, dim)
+
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Attention(dim, heads = heads, dim_head = dim_head),
+                FeedForward(dim)
+            ]))
+
+        self.to_logits = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, num_tokens)
+        )
+
+    def forward(self, x):
+        n, device = x.shape[1], x.device
+
+        x = self.token_emb(x)
+
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+
+        pos_emb = self.pos_emb(torch.arange(n, device = device))
+        x = x + rearrange(pos_emb, 'n d -> () n d')
+
+        return self.to_logits(x)
