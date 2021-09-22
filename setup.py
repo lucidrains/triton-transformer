f5028c63ebfa6b60889ef14169af9b8f9a9c7755
Phil Wang <lucidrains@gmail.com>
2021-09-22 15:47:22 -0700

    0.0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'triton-transformer',
   packages = find_packages(),
-  version = '0.0.7',
+  version = '0.0.8',
   license='MIT',
   description = 'Transformer in Triton',
   author = 'Phil Wang',
